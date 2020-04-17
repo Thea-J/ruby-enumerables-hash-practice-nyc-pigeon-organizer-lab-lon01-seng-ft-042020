ba59c339162b44c7bd4250ca4ca7ee9c03be9d4d
Dorothy John <dorothyy.john@gmail.com>
2020-04-17 07:42:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+hash = {}
+data.each do |tier1_attributes, values|
+values.each do |value, array|
+  array.each do |name|
+    if hash[name] == nil
+      hash[name] = {}
+      hash[name][tier1_attributes] = []
+    else 
+      hash[name][tier1_attributes] = []
+end
+end
+end
+end
+hash.each do |name, values|
+  values.each do |hashvalue, array|
+   data.each do |tier1_attributes, values|
+    values.each do |value, array|
+
+      array.each do |element|
+        if element == name && hashvalue == tier1_attributes
+          hash[name][hashvalue] << value.to_s
+        end
+      end
+    end
+  end
+end
+end
+hash
 end
